ca9b465c9ae43a57ef681f2a7d24f9062d84dba1
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-09-27 16:07:20 +0330

    feat: create class SelectImageToDelete

---
+++ MyEshop.Application/ViewModels/Image/SelectImageToDelete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Application.ViewModels.Image
+{
+    public class SelectImageToDelete
+    {
+        public SelectImageToDelete()
+        {
+
+        }
+        public SelectImageToDelete(Domain.Models.Image image)
+        {
+            ImageId = image.ImageId;
+            UrlImage = image.UrlImage;
+        }
+
+        public int ImageId { get; set; }
+        public string UrlImage { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
