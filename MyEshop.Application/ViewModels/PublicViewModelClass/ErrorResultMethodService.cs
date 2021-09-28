c4fa3a5790092a6f985576c6855372717db04708
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-09-28 17:11:09 +0330

    refactor: change name from MyEshop.Application.ViewModels.Public to MyEshop.Application.ViewModels.PublicViewModelClass

---
+++ MyEshop.Application/ViewModels/PublicViewModelClass/ErrorResultMethodService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Application.ViewModels.PublicViewModelClass
+{
+    public class ErrorResultMethodService
+    {
+        public ErrorResultMethodService()
+        {
+
+        }
+
+        public ErrorResultMethodService(string title, string message)
+        {
+            Title = title;
+            Message = message;
+        }
+
+        public string Title { set; get; }
+        public string Message { set; get; }
+    }
+}
