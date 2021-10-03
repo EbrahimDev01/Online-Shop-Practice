4cc253a2ecffce6a000e4e089e1e5b710bfb29ff
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-10-03 17:54:19 +0330

    refactor: change for input methods is null return true

--- MyEshop.Application/Utilities/File/FileHandler.cs
+++ MyEshop.Application/Utilities/File/FileHandler.cs
@@ -13,15 +13,13 @@ namespace MyEshop.Application.Utilities.File
 {
     public class FileHandler : IFileHandler
     {
-        public FileHandler()
-        {
-
-        }
 
         public async ValueTask<string> CreateAsync(IFormFile formFile, string newPath = null)
         {
-            if (formFile == null)
+            if (formFile is null)
+            {
                 return null;
+            }
 
             try
             {
@@ -60,6 +58,9 @@ namespace MyEshop.Application.Utilities.File
 
         public bool DeleteImages(IEnumerable<Domain.Models.Image> images)
         {
+            if (images is null)
+                return true;
+
             try
             {
                 foreach (var image in images)
@@ -84,85 +85,86 @@ namespace MyEshop.Application.Utilities.File
 
         public bool IsImage(IEnumerable<IFormFile> formFiles)
         {
-            foreach (var formFile in formFiles)
-            {
-                #region Check image
-
-                //-------------------------------------------
-                //  Check the image mime types
-                //-------------------------------------------
-                if (!string.Equals(formFile.ContentType, "image/jpg", StringComparison.OrdinalIgnoreCase) &&
-                        !string.Equals(formFile.ContentType, "image/jpeg", StringComparison.OrdinalIgnoreCase) &&
-                        !string.Equals(formFile.ContentType, "image/pjpeg", StringComparison.OrdinalIgnoreCase) &&
-                        !string.Equals(formFile.ContentType, "image/gif", StringComparison.OrdinalIgnoreCase) &&
-                        !string.Equals(formFile.ContentType, "image/x-png", StringComparison.OrdinalIgnoreCase) &&
-                        !string.Equals(formFile.ContentType, "image/png", StringComparison.OrdinalIgnoreCase))
+            if (formFiles is not null)
+                foreach (var formFile in formFiles)
                 {
-                    return false;
-                }
+                    #region Check image
 
-                //-------------------------------------------
-                //  Check the image extension
-                //-------------------------------------------
-                var formFileExtension = Path.GetExtension(formFile.FileName);
-                if (!string.Equals(formFileExtension, ".jpg", StringComparison.OrdinalIgnoreCase)
-                    && !string.Equals(formFileExtension, ".png", StringComparison.OrdinalIgnoreCase)
-                    && !string.Equals(formFileExtension, ".gif", StringComparison.OrdinalIgnoreCase)
-                    && !string.Equals(formFileExtension, ".jpeg", StringComparison.OrdinalIgnoreCase))
-                {
-                    return false;
-                }
-
-                //-------------------------------------------
-                //  Attempt to read the file and check the first bytes
-                //-------------------------------------------
-                try
-                {
-                    if (!formFile.OpenReadStream().CanRead)
-                    {
-                        return false;
-                    }
-                    //------------------------------------------
-                    //   Check whether the image size exceeding the limit or not
-                    //------------------------------------------ 
-                    if (formFile.Length < ImageMinimumBytes)
+                    //-------------------------------------------
+                    //  Check the image mime types
+                    //-------------------------------------------
+                    if (!string.Equals(formFile.ContentType, "image/jpg", StringComparison.OrdinalIgnoreCase) &&
+                            !string.Equals(formFile.ContentType, "image/jpeg", StringComparison.OrdinalIgnoreCase) &&
+                            !string.Equals(formFile.ContentType, "image/pjpeg", StringComparison.OrdinalIgnoreCase) &&
+                            !string.Equals(formFile.ContentType, "image/gif", StringComparison.OrdinalIgnoreCase) &&
+                            !string.Equals(formFile.ContentType, "image/x-png", StringComparison.OrdinalIgnoreCase) &&
+                            !string.Equals(formFile.ContentType, "image/png", StringComparison.OrdinalIgnoreCase))
                     {
                         return false;
                     }
 
-                    byte[] buffer = new byte[ImageMinimumBytes];
-                    formFile.OpenReadStream().Read(buffer, 0, ImageMinimumBytes);
-                    string content = System.Text.Encoding.UTF8.GetString(buffer);
-                    if (Regex.IsMatch(content, @"<script|<html|<head|<title|<body|<pre|<table|<a\s+href|<img|<plaintext|<cross\-domain\-policy",
-                        RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Multiline))
+                    //-------------------------------------------
+                    //  Check the image extension
+                    //-------------------------------------------
+                    var formFileExtension = Path.GetExtension(formFile.FileName);
+                    if (!string.Equals(formFileExtension, ".jpg", StringComparison.OrdinalIgnoreCase)
+                        && !string.Equals(formFileExtension, ".png", StringComparison.OrdinalIgnoreCase)
+                        && !string.Equals(formFileExtension, ".gif", StringComparison.OrdinalIgnoreCase)
+                        && !string.Equals(formFileExtension, ".jpeg", StringComparison.OrdinalIgnoreCase))
                     {
                         return false;
                     }
-                }
-                catch (Exception)
-                {
-                    return false;
-                }
 
-                //-------------------------------------------
-                //  Try to instantiate new Bitmap, if .NET will throw exception
-                //  we can assume that it's not a valid image
-                //-------------------------------------------
+                    //-------------------------------------------
+                    //  Attempt to read the file and check the first bytes
+                    //-------------------------------------------
+                    try
+                    {
+                        if (!formFile.OpenReadStream().CanRead)
+                        {
+                            return false;
+                        }
+                        //------------------------------------------
+                        //   Check whether the image size exceeding the limit or not
+                        //------------------------------------------ 
+                        if (formFile.Length < ImageMinimumBytes)
+                        {
+                            return false;
+                        }
 
-                try
-                {
-                    using var bitmap = new Bitmap(formFile.OpenReadStream());
+                        byte[] buffer = new byte[ImageMinimumBytes];
+                        formFile.OpenReadStream().Read(buffer, 0, ImageMinimumBytes);
+                        string content = System.Text.Encoding.UTF8.GetString(buffer);
+                        if (Regex.IsMatch(content, @"<script|<html|<head|<title|<body|<pre|<table|<a\s+href|<img|<plaintext|<cross\-domain\-policy",
+                            RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Multiline))
+                        {
+                            return false;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+
+                    //-------------------------------------------
+                    //  Try to instantiate new Bitmap, if .NET will throw exception
+                    //  we can assume that it's not a valid image
+                    //-------------------------------------------
+
+                    try
+                    {
+                        using var bitmap = new Bitmap(formFile.OpenReadStream());
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                    finally
+                    {
+                        formFile.OpenReadStream().Position = 0;
+                    }
+                    #endregion
                 }
-                catch (Exception)
-                {
-                    return false;
-                }
-                finally
-                {
-                    formFile.OpenReadStream().Position = 0;
-                }
-                #endregion
-            }
             return true;
         }
     }
