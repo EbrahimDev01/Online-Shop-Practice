dfbe708444c02bf3d55f0b40980385dd860d1c24
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-10-06 16:02:52 +0330

    feat: add class test TagManagerControllerTest

---
+++ MyEshop.Test/ControllersTest/Admin/TagManagerControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Test.ControllersTest.Admin
+{
+    public class TagManagerControllerTest
+    {
+    }
+}
