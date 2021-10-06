2537ba2d13d0797214286056a5fdf1350969f743
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-10-06 16:11:53 +0330

    test: testing for check index result contains value

--- MyEshop.Test/ControllersTest/Admin/TagManagerControllerTest.cs
+++ MyEshop.Test/ControllersTest/Admin/TagManagerControllerTest.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MyEshop.Application.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace MyEshop.Test.ControllersTest.Admin
 {
     public class TagManagerControllerTest
     {
+        private readonly TagManagerController _tagManagerController;
+        private readonly Mock<ITagService> _mockTagService;
+
+        public TagManagerControllerTest()
+        {
+            _mockTagService = new Mock<ITagService>();
+
+            _tagManager = new TagManagerController(_mockTagService.Object);
+        }
+
+        [Fact]
+        public async Task Test_Index_Result_Contains_Value()
+        {
+            var resultProductIndex = _tagManagerController.Index() as ViewResult;
+
+            var resultProductIndexModel = resultProductIndex.Model as TagViewModel;
+
+            Assert.NotNull(resultProductIndex);
+            Assert.NotNull(resultProductIndexModel);
+        }
     }
 }
