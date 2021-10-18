839585d4e7d5e6a33ee3354250d317f5cd8307f7
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-10-18 16:40:16 +0330

    test: check create tag when title is exist

--- MyEshop.Test/ControllersTest/Admin/TagManagerControllerTest.cs
+++ MyEshop.Test/ControllersTest/Admin/TagManagerControllerTest.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Moq;
+using MyEshop.Application.ConstApplication.Names;
 using MyEshop.Application.Interfaces;
+using MyEshop.Application.ViewModels.PublicViewModelClass;
 using MyEshop.Application.ViewModels.Tag;
+using MyEshop.Domain.ConstsDomain.Messages;
 using MyEshop.Mvc.Areas.Admin.Controllers;
 using System;
 using System.Collections.Generic;
@@ -73,6 +76,25 @@ namespace MyEshop.Test.ControllersTest.Admin
             var resultTagCreate = _tagManagerController.Create(new TagCreateViewModel()) as ViewResult;
 
             Assert.NotNull(resultTagCreate);
+        }
+
+        [Fact]
+        public void Test_Create_Result_Is_Not_Exist_Tag()
+        {
+            _mockTagService.Setup(tagService => tagService.IsExistTagByTitle(It.IsAny<string>()))
+                .ResultAsync(false);
+
+            var resultTagCreate = _tagManagerController.Create(new TagCreateViewModel()) as ViewResult;
+
+            var resultTagCreateErrors = _tagManagerController.ModelState.Where(y => y.Value.Errors.Count > 0)
+                        .Select(x => new ErrorResultMethodService(x.Key, x.Value.Errors.FirstOrDefault().ErrorMessage));
+
+            Assert.NotNull(resultTagCreate);
+            Assert.Equal(1, _tagManagerController.ModelState.ErrorCount);
+            Assert.False(_tagManagerController.ModelState.IsValid);
+            Assert.Contains(resultTagCreateErrors,
+                error => error.Title == nameof(TagCreateViewModel.Title) &&
+                error.Message == ErrorMessage.IsExistWithName(DisplayNames.Tag));
         }
     }
 }
