b59e4bac3bef2a1d8f34df8d2249a722e9cc05b4
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-10-24 21:30:07 +0330

    feat: create class for most used names

---
+++ MyEshop.Application/Utilities/ConstApplication/Names/MostUsedNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Application.Utilities.ConstApplication.Names
+{
+    public static class MostUsedNames
+    {
+        public const string RequestVerificationToken = "__RequestVerificationToken";
+    }
+}
