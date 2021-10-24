ad2ff3054478b03c2081f2d4c6cd6d69900e9795
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-10-24 21:32:09 +0330

    feat: add remote to title tag for check before submit form is exist

--- MyEshop.Application/ViewModels/Tag/TagCreateViewModel.cs
+++ MyEshop.Application/ViewModels/Tag/TagCreateViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MyEshop.Application.ConstApplication.Names;
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEshop.Application.ConstApplication.Names;
+using MyEshop.Application.Utilities.ConstApplication.Names;
 using MyEshop.Domain.ConstsDomain.Messages;
 using System;
 using System.Collections.Generic;
@@ -14,6 +16,10 @@ namespace MyEshop.Application.ViewModels.Tag
         [Display(Name = DisplayNames.Title)]
         [Required(ErrorMessage = ErrorMessage.Required)]
         [MaxLength(150, ErrorMessage = ErrorMessage.MaxLength)]
+        [Remote("IsExistTagByTitle", "TagManager", "Admin",
+            HttpMethod = "POST",
+            AdditionalFields = MostUsedNames.RequestVerificationToken,
+            ErrorMessage = ErrorMessage.IsExist)]
         public string Title { get; set; }
     }
 }
