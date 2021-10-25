d6739c1668847cec1633cf3c747a51548bc365a6
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-10-25 19:10:06 +0330

    feat: add view model TagDetailsViewModel

---
+++ MyEshop.Application/ViewModels/Tag/TagDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Application.ViewModels.Tag
+{
+    public class TagDetailsViewModel : TagViewModel
+    {
+        public TagDetailsViewModel()
+        {
+
+        }
+
+        public TagDetailsViewModel(Domain.Models.Tag tag, IAsyncEnumerable<Product.PreviewAdminProductViewModel> previewAdminProducts)
+            : base(tag)
+        {
+            PreviewAdminProducts = previewAdminProducts;
+        }
+
+        public IAsyncEnumerable<Product.PreviewAdminProductViewModel> PreviewAdminProducts { set; get; }
+    }
+}
