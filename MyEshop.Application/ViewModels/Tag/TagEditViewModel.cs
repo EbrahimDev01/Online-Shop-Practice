bbcad4bcd20c700e08b383f0fc45a4a4101d48e2
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-11-02 18:04:31 +0330

    feat: create TagEditViewModel

---
+++ MyEshop.Application/ViewModels/Tag/TagEditViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEshop.Application.ConstApplication.Names;
+using MyEshop.Application.Utilities.ConstApplication.Names;
+using MyEshop.Domain.ConstsDomain.Messages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Application.ViewModels.Tag
+{
+    public class TagEditViewModel
+    {
+
+        public TagEditViewModel()
+        {
+
+        }
+        public TagEditViewModel(Domain.Models.Tag tag)
+        {
+            TagId = tag.TagId;
+            Title = tag.Title;
+        }
+
+        public int TagId { get; set; }
+
+        [Display(Name = DisplayNames.Title)]
+        [Required(ErrorMessage = ErrorMessage.Required)]
+        [MaxLength(150, ErrorMessage = ErrorMessage.MaxLength)]
+        [Remote("IsExistTagByTitleAndId", "TagManager", "Admin",
+            HttpMethod = "POST",
+            AdditionalFields = MostUsedNames.RequestVerificationToken + "," + nameof(TagId),
+            ErrorMessage = ErrorMessage.IsExist)]
+        public string Title { get; set; }
+    }
+}
