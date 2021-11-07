16e27c686f7721ce356ff3d464bd00d87e34b424
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-11-07 18:01:00 +0330

    feat: create class tag delete view model

---
+++ MyEshop.Application/ViewModels/Tag/TagDeleteViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MyEshop.Application.ViewModels.Tag
+{
+    public class TagDeleteViewModel : TagViewModel
+    {
+        public TagDeleteViewModel()
+        {
+
+        }
+        public TagDeleteViewModel(Domain.Models.Tag tag)
+            : base(tag)
+        {
+        }
+    }
+}
