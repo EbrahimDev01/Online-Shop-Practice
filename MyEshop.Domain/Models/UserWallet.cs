705e0fa236709b9e4b144644fbfe94b152310fc2
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-11-13 18:18:08 +0330

    feat: create entity UserWallet

---
+++ MyEshop.Domain/Models/UserWallet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Domain.Models
+{
+    public class UserWallet
+    {
+        public decimal WalletBalance { set; get; }
+
+        public virtual ICollection<UserWalletDetails> UserWalletDetails { set; get; }
+
+        public virtual ApplicationUser ApplicationUser { set; get; }
+
+    }
+}
