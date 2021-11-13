1fbb274b37270912a975dfaaaff1af69751b6b2c
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-11-13 18:17:23 +0330

    feat: create entity ApplicationUser

---
+++ MyEshop.Domain/Models/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Domain.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string NationalCode { set; get; }
+        public DateTime Birthday { set; get; }
+
+        #region Relations
+
+        public int UserImageId { set; get; }
+        public virtual Image UserImage { set; get; }
+
+        public virtual ICollection<Comment> UserComments { set; get; } = new List<Comment>();
+
+        public virtual ICollection<Cart> UserCarts { set; get; } = new List<Cart>();
+
+        public virtual ICollection<UserAddress> UserAddresses { set; get; } = new List<UserAddress>();
+
+        public int UserWalletId { set; get; }
+        public virtual UserWallet UserWallet { set; get; }
+        #endregion
+    }
+}
