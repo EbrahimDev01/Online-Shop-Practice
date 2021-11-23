ec029cb44e0f3b4bf6581f4671596b0a4a7af7a9
EbrahimDev01 <EbrahimDev01@gmail.com>
2021-11-23 17:50:42 +0330

    feat: create file FluentCategoryConfig

---
+++ MyEshop.Data/FluentConfigs/FluentCategoryConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyEshop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Data.FluentConfigs
+{
+    public class FluentCategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(category => category.CategoryId);
+
+            builder.Property(category => category.Title)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.HasMany(category => category.CategoriesChildren)
+                .WithOne(category => category.CategoryParent)
+                .HasForeignKey(category => category.CategoryParentId);
+        }
+    }
+}
