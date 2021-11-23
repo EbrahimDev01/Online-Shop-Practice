bca8120387bd73c2fbe042e68e0105558850e5f5
EbrahimDev01 <EbrahimDev01@gmail.com>
2021-11-23 17:53:24 +0330

    feat: create file FluentUserWalletDetailsConfig

---
+++ MyEshop.Data/FluentConfigs/FluentUserWalletDetailsConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyEshop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Data.FluentConfigs
+{
+    class FluentUserWalletDetailsConfig : IEntityTypeConfiguration<UserWalletDetails>
+    {
+        public void Configure(EntityTypeBuilder<UserWalletDetails> builder)
+        {
+            builder.HasKey(userWalletDetails => userWalletDetails.UserWalletDetailsId);
+        }
+    }
+}
