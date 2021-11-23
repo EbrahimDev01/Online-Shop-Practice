e490683e65dc7c20d8baf0de90e209cb2ab67caa
EbrahimDev01 <EbrahimDev01@gmail.com>
2021-11-23 17:51:12 +0330

    feat: create file FluentCommentConfig

---
+++ MyEshop.Data/FluentConfigs/FluentCommentConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyEshop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Data.FluentConfigs
+{
+    public class FluentCommentConfig : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasKey(comment => comment.CommentId);
+
+            builder.Property(comment => comment.Title)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(comment => comment.Title)
+                .HasMaxLength(500)
+                .IsRequired();
+        }
+    }
+}
