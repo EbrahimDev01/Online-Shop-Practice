2a055f0ccf03250b199b5c8e6b7de5a6b5755663
EbrahimDev01 <EbrahimDev01@gmail.com>
2021-11-23 17:58:55 +0330

    change relations instance from list to hash set

--- MyEshop.Domain/Models/ApplicationUser.cs
+++ MyEshop.Domain/Models/ApplicationUser.cs
@@ -17,11 +17,11 @@ namespace MyEshop.Domain.Models
         public int? UserImageId { set; get; }
         public virtual Image UserImage { set; get; }
 
-        public virtual ICollection<Comment> UserComments { set; get; } = new List<Comment>();
+        public virtual ICollection<Comment> UserComments { set; get; } = new HashSet<Comment>();
 
-        public virtual ICollection<Cart> UserCarts { set; get; } = new List<Cart>();
+        public virtual ICollection<Cart> UserCarts { set; get; } = new HashSet<Cart>();
 
-        public virtual ICollection<UserAddress> UserAddresses { set; get; } = new List<UserAddress>();
+        public virtual ICollection<UserAddress> UserAddresses { set; get; } = new HashSet<UserAddress>();
 
         public int UserWalletId { set; get; }
         public virtual UserWallet UserWallet { set; get; }
