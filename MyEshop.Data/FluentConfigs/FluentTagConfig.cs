07ad31d0071475fa46e5022712e61e28417bd88d
EbrahimDev01 <EbrahimDev01@gmail.com>
2021-11-23 17:51:54 +0330

    feat: create file FluentTagConfig

---
+++ MyEshop.Data/FluentConfigs/FluentTagConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyEshop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Data.FluentConfigs
+{
+    public class FluentTagConfig : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.HasKey(tag => tag.TagId);
+
+            builder.Property(tag => tag.Title)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
