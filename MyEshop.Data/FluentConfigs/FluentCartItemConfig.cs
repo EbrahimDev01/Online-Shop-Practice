97d71f1dbb4e4910b5a809e4eeee9eba45a87b6e
EbrahimDev01 <EbrahimDev01@gmail.com>
2021-11-23 17:50:18 +0330

    feat: create file FluentCartItemConfig

---
+++ MyEshop.Data/FluentConfigs/FluentCartItemConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyEshop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Data.FluentConfigs
+{
+    public class FluentCartItemConfig : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.HasKey(cartItem => cartItem.CartItemId);
+        }
+    }
+}
