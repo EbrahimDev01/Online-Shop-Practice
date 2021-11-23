aa88d4c407decbbdfd784509c4acbe72634df0cd
EbrahimDev01 <EbrahimDev01@gmail.com>
2021-11-23 17:51:35 +0330

    feat: create file FluentImageConfig

---
+++ MyEshop.Data/FluentConfigs/FluentImageConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyEshop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.Data.FluentConfigs
+{
+    public class FluentImageConfig : IEntityTypeConfiguration<Image>
+    {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            builder.HasKey(image => image.ImageId);
+
+            builder.Property(image => image.UrlImage)
+                .IsRequired();
+            builder.Property(image => image.NameImage)
+                .IsRequired();
+        }
+    }
+}
