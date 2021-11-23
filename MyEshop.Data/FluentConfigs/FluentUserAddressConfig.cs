03d075b09780910c0657e966aac7bd3c7b131076
EbrahimDev01 <EbrahimDev01@gmail.com>
2021-11-23 17:52:35 +0330

    feat: create file FluentUserAddressConfig

---
+++ MyEshop.Data/FluentConfigs/FluentUserAddressConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyEshop.Domain.Models;
+
+namespace MyEshop.Data.FluentConfigs
+{
+    public class FluentUserAddressConfig : IEntityTypeConfiguration<UserAddress>
+    {
+        public void Configure(EntityTypeBuilder<UserAddress> builder)
+        {
+            builder.HasKey(userAddress => userAddress.UserAddressId);
+
+            builder.Property(userAddress => userAddress.State)
+                .IsRequired();
+
+            builder.Property(userAddress => userAddress.PostalCard)
+                .IsRequired();
+
+            builder.Property(userAddress => userAddress.City)
+                .IsRequired();
+
+            builder.Property(userAddress => userAddress.PostalAddress)
+                .IsRequired();
+
+            builder.Property(userAddress => userAddress.Plaque)
+                .IsRequired();
+
+            builder.Property(userAddress => userAddress.UnitNumber)
+                .IsRequired();
+        }
+    }
+}
