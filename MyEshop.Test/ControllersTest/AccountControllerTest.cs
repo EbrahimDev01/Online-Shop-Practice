5e1518ccd70bb95bfcdf2583c8ba59cc404baa1e
EbrahimDev01 <EbrahimDev01@gmail.com>
2021-11-26 16:40:27 +0330

    test: create account controller

---
+++ MyEshop.Test/ControllersTest/AccountControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using MyEshop.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyEshop.Test.ControllersTest
+{
+    internal class AccountControllerTest
+    {
+        private readonly AccountController _accountController;
+
+        public AccountControllerTest(AccountController accountController)
+        {
+            _accountController = _accountController
+        }
+    }
+}
